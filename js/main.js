6c6042d64ed7f45975fd91ef060c40d1b8733ce8
Allen Guo <guoguo12@gmail.com>
2017-03-22 07:09:20 -0700

    Overhaul front-end
    Vue.js is great! I'm never using Angular ever again!

--- js/main.js
+++ js/main.js
@@ -1,210 +1,177 @@
-// MAIN.JS -- Hivemind
-// Allen Guo <allenguo@berkeley.edu>
+var DATA_URL = 'https://hivemind-data.firebaseapp.com/latest.json';
 
-// var DATA_URL = "test.json";
-var DATA_URL = "https://hivemind-data.firebaseapp.com/latest.json";
+var OFFLINE = false;
 
-var RATING_TEXTS = {
-  1: "Low",
-  2: "Moderate",
-  3: "High",
-  4: "Unavailable"
+var RATING_TEXTS = ['Low', 'Moderate', 'High', 'Unavailable'];
+
+var USERS_SHOWN = 8;
+
+var DEFAULT_SORTING = 'rating';
+
+var CLIPBOARD_COPY_TEXT = 'Copied!';
+var CLIPBOARD_ERROR_TEXT = 'Failed (browser unsupported)';
+
+// See RATING_TEXTS above for explanations of rating values
+var toRating = function(usage, userCount) {
+  if (usage >= 0.7 || userCount >= 30) {
+    return 2;
+  } else if (usage >= 0.3 || userCount >= 15) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
 
-// load is CPU usage as a percentage
-// userCount is the number of users
-function toRating(load, userCount) {
-  if (load >= 70 || userCount >= 30) {
-    var rating = 3; // High
-  } else if (load >= 30 || userCount >= 15) {
-    var rating = 2; // Moderate
-  } else if (load >= 0 || userCount >= 0) {
-    var rating = 1; // Low
-  } else {
-    var rating = 4; // Unavailable
-  }
-  return '<span class="rating rating-' + rating + '">&#9679; ' + RATING_TEXTS[rating] + '</span>';
-}
-
-function toHumanDuration(seconds) {
-  return moment.duration(seconds, 'seconds').humanize();
-}
-
-function toHumanUserCount(count) {
-  if (count == 1) {
-    return '1 user';
-  }
-  return count + ' users';
-}
-
-function updateLastUpdated(lastUpdated) {
-  var timeString = lastUpdated.format("h:mm A, L");
-  var humanReadable = lastUpdated.fromNow();
-  $('#updated').html('The data below was gathered <b>' + humanReadable + '</b>.');
-  $('#loading').html('Refresh to check for updates.');
-}
-
-function update() {
+var update = function() {
   $.getJSON(DATA_URL, function(data) {
     console.log('Generation time: ' + data.time_elapsed + 's');
 
-    $('#data-body').html('');
-
-    var lastUpdate = moment.unix(data.time_begin + data.time_elapsed);
-    updateLastUpdated(lastUpdate);
-
-    var totalLoad = 0;
-    var totalLoadCount = 0;
-    // data.data holds the data for the servers
-    Object.keys(data.data).forEach(function(server) {
-      if (server === '') {
+    // Transform JSON data into array of server objects
+    var servers = [];
+    Object.keys(data.data).forEach(function(name) {
+      if (name === '') {
         return;
       }
 
-      var displayName = server.split('.')[0];
-      var serverHtml = '<span data-full-name="' + server + '">' + displayName + '</span>';
-      var serverData = data.data[server];
-      if (Object.keys(serverData).length === 0) {
-        var items = [serverHtml,
-                     toRating(-1, -1),
-                     '&mdash;',
-                     '&mdash;'];
+      var server = {};
+      server.name = name;
+      server.shortName = name.split('.')[0];
+      if (server.shortName.match(/\D*\d$/i)) {  // Ends with single digit
+        // Add a zero in the tens place to make sorting human-friendly
+        server.sortName = server.shortName.slice(0, -1) + '0' + server.shortName.slice(-1);
       } else {
-        var userCount = serverData.users.length;
-        var load = serverData.load_avgs[1] * 100;
-        totalLoad += load;
-        totalLoadCount++;
-        var loadString = parseFloat(load).toFixed(2) + '%';
-        if (userCount > 8) {
-          var userList = serverData.users.slice(0, 8).join(', ') + ', and ' + (userCount - 8) + ' more';
-          var userCountHtml = '<span class="dashed hint--bottom" data-hint="' + userList + '">' + toHumanUserCount(userCount) + '</span>';
-        } else if (userCount > 0) {
-          var userCountHtml = '<span class="dashed hint--bottom" data-hint="' + serverData.users.join(', ') + '">' + toHumanUserCount(userCount) + '</span>';
-        } else {
-          var userCountHtml = toHumanUserCount(userCount);
-        }
-        var items = [serverHtml,
-                     toRating(load, userCount),
-                     userCountHtml,
-                     loadString];
+        server.sortName = server.shortName;
       }
-      var html = '<tr><td>' + items.join('</td><td>') + '</td></tr>';
-      $('#data-body').append(html);
+
+      var jsonData = data.data[name];
+      if ($.isEmptyObject(jsonData)) {
+        // Many of these properties are used for sorting
+        server.available = false;
+        server.usage = Infinity;
+        server.users = [];
+        server.userCount = -1;
+        server.rating = 3;
+      } else {
+        server.available = true;
+        server.usage = jsonData.load_avgs[1];
+        server.users = jsonData.users;
+        server.userCount = server.users.length;
+        server.rating = toRating(server.usage, server.userCount);
+      }
+
+      servers.push(server);
     });
+    app.servers = servers;
 
-    $('table').tablesorter({sortList: [[1,0], [3,0]], headers: {0: { sorter: 'servers'},
-                                                                1: { sorter: 'ratings'},
-                                                                2: { sorter: 'users'  },
-                                                                3: { sorter: 'loads'  }}});
-    $('table').removeClass('hidden');
-    updateQuickStatsBox(totalLoad / totalLoadCount);
-    addAllClipboards();
+    app.lastUpdate = data.time_begin + data.time_elapsed;
+    app.dataLoaded = true;
   }).fail(function() {
-    $('#loading')
-      .html('Couldn\'t retrieve Hivemind data file. Try again later?')
-      .css('color', '#D32F2F');
+    app.failed = true;
   });
+};
+
+var app = new Vue({
+  el: '#main',
+  data: {
+    dataLoaded: false,
+    failed: false,
+    offline: OFFLINE,
+
+    lastUpdate: '',
+    servers: [],
+
+    search: '',
+    orderBy: DEFAULT_SORTING,
+    reverse: false
+  },
+  filters: {
+    timeSince: function(time) {
+      return moment.unix(time).fromNow();
+    },
+    percent: function(value) {
+      return parseFloat(value * 100).toFixed(2);
+    },
+    ratingText: function(rating) {
+      return RATING_TEXTS[rating];
+    },
+    userCount: function(count) {
+      return count === 1 ? ' 1 user' : count + ' users';
+    },
+    userList: function(users) {
+      var count = users.length;
+      if (count > USERS_SHOWN) {
+        return users.slice(0, USERS_SHOWN).join(', ') + ', and ' + (count - USERS_SHOWN) + ' more';
+      }
+      return users.join(', ');
+    }
+  },
+  computed: {
+    orderedServers: function() {
+      var key = this.orderBy;
+      return this.servers.sort(function(a, b) {
+        if (a[key] > b[key]) {
+          return 1;
+        } else if (a[key] < b[key]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    },
+    reversedServers: function() {
+      return this.reverse ? this.orderedServers.reverse() : this.orderedServers;
+    },
+    filteredServers: function() {
+      var search = this.search;
+      if (search === '') {
+        return this.reversedServers;
+      }
+      return this.reversedServers.filter(function(server) {
+        return server.name.match(search);
+      });
+    }
+  },
+  methods: {
+    headerClass: function(columnName) {
+      return {
+        headerSortDown: this.orderBy === columnName && !this.reverse,
+        headerSortUp: this.orderBy === columnName && this.reverse
+      };
+    },
+    headerClick: function(columnName) {
+      if (this.orderBy === columnName) {
+        this.reverse = !this.reverse;
+      } else {
+        this.orderBy = columnName;
+      }
+    }
+  },
+  watch: {
+    filteredServers: activateAllClipboards
+  }
+});
+
+function activateAllClipboards() {
+  // TODO: Get the timing to work without setTimeout
+  setTimeout(function() {
+    $('tr td:first-child span').each(function(i, elem) {
+      activateClipboard(elem);
+    });
+  }, 100);
 }
 
-function updateQuickStatsBox(avgLoad) {
-  var bestHiveServer = $('table td span').filter(function(i, e) { return e.innerText.indexOf('hive') !== -1; }).attr('data-full-name') + '.berkeley.edu';
-  activateClipboard(bestHiveServer, '#best-hive');
-  $('#best-hive').html(bestHiveServer);
-
-  var bestServer = $('table td span').html() + '.berkeley.edu';
-  activateClipboard(bestServer, '#best');
-  $('#best').html(bestServer);
-
-  $('#stats').removeClass('hidden');
-}
-
-function addAllClipboards() {
-  $('tr td:first-child span').each(function(i, elem) {
-    activateClipboard($(elem).attr('data-full-name') + '.berkeley.edu', elem);
-  });
-}
-
-function activateClipboard(copyText, sel) {
+function activateClipboard(sel) {
   var clipboard = new Clipboard(sel);
-  $(sel)
-    .attr('data-clipboard-text', copyText)
-    .addClass('dashed hint--bottom')
-    .addClass('click')
-    .attr('data-hint', 'Click to copy address');
-
   clipboard.on('success', function(e) {
-    $(sel).attr('data-hint', 'Copied!');
+    var origAttr = $(sel).attr('data-hint');
+    $(sel).attr('data-hint', CLIPBOARD_COPY_TEXT);
     setTimeout(function() {
-      $(sel).attr('data-hint', 'Click to copy address');
-    }, 1000); // 1 second
+      $(sel).attr('data-hint', origAttr);
+    }, 1000);  // 1 second
   });
   clipboard.on('error', function(e) {
-    $(sel).attr('data-hint', 'Failed (browser unsupported)');
+    $(sel).attr('data-hint', CLIPBOARD_ERROR_TEXT);
   });
 }
 
-function configParsers() {
-  $.tablesorter.addParser({ 
-    id: 'servers', 
-    is: function(s) { 
-      return false; 
-    },
-    format: function(s) { 
-      if (s.match(/.*?[a-z]|\-\d$/i)) { // Only one digit at end, e.g. hive1
-        return s.slice(0, -1) + '0' + s.slice(-1);
-      }
-      return s;
-    },
-    type: 'text'
-  });
-  $.tablesorter.addParser({ 
-    id: 'ratings', 
-    is: function(s) { 
-      return false; 
-    },
-    format: function(s) {
-      var ratingString = s.split(' ')[1];
-      for (var ratingNum in RATING_TEXTS) {
-        if (RATING_TEXTS[ratingNum] === ratingString) {
-          return ratingNum;
-        }
-      }
-      return 0;
-    },
-    type: 'numeric'
-  });  
-  $.tablesorter.addParser({ 
-    id: 'users', 
-    is: function(s) { 
-      return false; 
-    },
-    format: function(s) { 
-      var m = s.match(/(\d+) Users?/i);
-      if (m) {
-        return parseInt(m[1]);
-      }
-      return 1000;
-    },
-    type: 'numeric'
-  });  
-  $.tablesorter.addParser({ 
-    id: 'loads', 
-    is: function(s) { 
-      return false; 
-    },
-    format: function(s) { 
-      if (s.indexOf('—') !== -1) {
-        return 1000;
-      }
-      return parseFloat(s);
-    },
-    type: 'numeric'
-  });
-}
-
-function main() {
-  configParsers();
-  update();
-}
-
-$(main);
+$(update);
